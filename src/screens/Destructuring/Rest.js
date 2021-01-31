b5677166c8c830a9963de93fe30a0f9ff9f88d11
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-01-31 20:46:20 +0530

    rest example

--- src/screens/Destructuring/Rest.js
+++ src/screens/Destructuring/Rest.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+const restOperationTest = (...args) => {
+    console.log(args);
+};
+
+const restTestTwo = (a, b, ...args) => {
+    console.log(`a: ${a}`);
+    console.log(`b: ${b}`);
+    console.log(`args: ${args}`);
+}
+
 const Rest = ({
     params,
 }) => {
+
+    restOperationTest(1, 2, 3, 4, 5);
+
+    restTestTwo(1, 2, 3,4,5,6);
+
     return (
         <View>
             <Text>Rest</Text>
